caa5081420a405e35d3fcc062d76977544929d53
IsmaLga <a01630333@itesm.mx>
2015-03-02 11:42:37 -0600

    Create wsq03.py

---
+++ wsq03.py
@@ -0,0 +1,13 @@
+a = int(input("ingresa un numero"))
+b = int (input("ingresa otro numero"))
+
+sum = a + b
+dif = a - b
+div = a//b
+prod = a*b
+rem = a%b
+print ("the sum of both numbers is ",sum)
+print ("the susbtraction of both numbers is ",dif)
+print ("the division of both numbers is ",div)
+print ("the remainder of the divison of both numbers is ",rem)
+print ("the multiplication of both numbers is ",prod)
