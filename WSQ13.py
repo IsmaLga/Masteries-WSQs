70ee878c0e433ed0607827c4a8f5a0bde0d6e7d4
IsmaLga <a01630333@itesm.mx>
2015-04-09 20:32:48 -0500

    Update WSQ13.py

--- WSQ13.py
+++ WSQ13.py
@@ -6,14 +6,12 @@ def squareroot(n):
 		y=x
 		x=(x+z)/2
 	return x
-ans="yes"
-while (ans=="yes"):
-	n=float(input("Give me a number: "))
-	if (n==0):
-		print("The square root is: 0")
-	elif (n<0):
-		print("Error")
-	else:
-		sqrt=squareroot(n)
-		print("The square root is: ", sqrt)
-	ans=str(input("Do you want to try another number?"))
+
+n=float(input("Give me a number: "))
+if (n==0):
+	print("The square root is: 0")
+elif (n<0):
+	print("Error")
+else:
+	sqrt=squareroot(n)
+	print("The square root is: ", sqrt)
